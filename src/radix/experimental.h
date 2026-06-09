c0e46623b55f9250bce3e020133b6f64bc539ad9
AwardOfSky <fjrbaeta@gmail.com>
2026-06-09 03:18:11 +0100

    improvements for future reflection

--- src/radix/experimental.h
+++ src/radix/experimental.h
@@ -62,9 +62,11 @@ struct fixed_string {
 };
 
 
-// get key machinery
+// ---- reflection helpers ----
+
+// find member in type, return reflection info
 template<typename T, fixed_string Name>
-consteval auto reflection_find_member() {
+consteval std::meta::info refl_get_member() {
     constexpr auto ctx = std::meta::access_context::current();
 
     for (auto m : std::meta::nonstatic_data_members_of(^^T, ctx)) {
@@ -72,11 +74,59 @@ consteval auto reflection_find_member() {
             return m;
         }
     }
+
     return std::meta::info{};
 }
 
+// return reflection info of all members of type except first "member"
+template<typename T, fixed_string Name>
+consteval std::vector<std::meta::info> refl_getallbut_member() {
+    constexpr auto ctx = std::meta::access_context::current();
+    std::vector<std::meta::info> data_fields;        
+
+    for (std::meta::info m : std::meta::nonstatic_data_members_of(^^T, ctx)) {
+        if (std::meta::identifier_of(m) != Name.view()) {   
+            data_fields.push_back(m);
+        }
+    }
+
+    return data_fields;
+}
+
+// return reflected data for a member with type and identifier m
+consteval std::meta::info refl_meta_to_member(std::meta::info m) {
+    return std::meta::data_member_spec(
+        std::meta::type_of(m),
+        {.name = std::meta::identifier_of(m)}
+    );
+}
+
+// transform array of metadata into members, according to refl_meta_to_member
+consteval std::vector<std::meta::info>
+refl_metavec_to_members(std::vector<std::meta::info> arr) {
+    for (std::meta::info& m : arr) {
+        m = refl_meta_to_member(m);
+    }
+
+    return arr;
+}
+
 template<typename T>
-using ptr_t = T*;
+using refl_ptr_t = T*;
+
+// return reflection of a pointer to type
+template<typename T>
+consteval std::meta::info refl_ptr() {
+    return ^^refl_ptr_t<T>;
+}
+
+// reflect metadata into a C++ type
+template<std::meta::info meta>
+using refl_meta_to_type = typename[: std::meta::type_of(meta) :];
+
+// reflect metadata of a type into a C++ type
+template<std::meta::info meta>
+using refl_metatype_to_type = typename[: meta :];
 
 
 /*
@@ -104,26 +154,13 @@ struct soa_traits {
 
 
     static consteval std::meta::info get_key() {
-        return reflection_find_member<Pair, Key>();
+        return refl_get_member<Pair, Key>();
     }
 
     consteval {
-        constexpr auto ctx = std::meta::access_context::current();
+        auto value_fields = refl_metavec_to_members(refl_getallbut_member<Pair, Key>());
+        
         constexpr std::meta::info key_meta = get_key();
-
-        std::vector<std::meta::info> value_fields;        
-
-        for (std::meta::info m : std::meta::nonstatic_data_members_of(^^Pair, ctx)) {
-            if (std::meta::identifier_of(m) != Key.view()) {   
-                value_fields.push_back(
-                    std::meta::data_member_spec(
-                        std::meta::type_of(m),
-                        {.name = std::meta::identifier_of(m)}
-                    )
-                );
-            }
-        }
-
         static_assert(
             key_meta != std::meta::info{},
             "[soa_traits]: key name not found in type!"
@@ -133,11 +170,11 @@ struct soa_traits {
 
         std::vector<std::meta::info> soa_fields = {
             std::meta::data_member_spec(
-                ^^value_type*,
+                refl_ptr<value_type>(),
                 {.name = "value"}
             ),
             std::meta::data_member_spec(
-                std::meta::substitute(^^ptr_t, {std::meta::type_of(key_meta)}),
+                refl_ptr<refl_meta_to_type<key_meta>>(),
                 {.name = std::meta::identifier_of(key_meta)}
             )
         };
